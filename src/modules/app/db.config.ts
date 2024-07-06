a3cb59526a97ba913704d686933aee1bd8ef4ed2
l1agosta <recommendation082@gmail.com>
2024-07-06 22:14:02 +0500

    fixed db.config.ts

--- src/modules/app/db.config.ts
+++ src/modules/app/db.config.ts
@@ -7,7 +7,7 @@ dotenv.config();
 export const DB_CONFIG: SequelizeModuleOptions = {
     dialect: 'postgres',
     host: process.env.DB_HOST,
-    port: Number(process.env.DB_PORT) || 5432,
+    port: Number(process.env.DB_PORT),
     username: process.env.DB_USERNAME,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
